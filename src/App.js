5e538bef0c2bc78363380e607b1ebc48b50cbd36
eyjoprogrammers <eyjoprogrammers@gmail.com>
2025-07-13 09:17:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
         <main className="flex-grow">
                      <Route path="/" element={<Home />} />
 
-    <Routes>
+    <Routes>                     <Route path="/" element={<Home />} />
+
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
