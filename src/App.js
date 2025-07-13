6d372414b55481a05ba017b99f1ddbaa2b801cfd
eyjoprogrammers <eyjoprogrammers@gmail.com>
2025-07-13 09:14:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
       <div className="flex flex-col min-h-screen " dir="rtl">
         <Header />
         <main className="flex-grow">
-          <Routes>
+                     <Route path="/" element={<Home />} />
+
+    <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
