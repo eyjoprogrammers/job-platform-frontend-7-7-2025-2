a41dd715a15148321f76144021b8fa36033b27fd
Ey Jo <eyjoprogrammers@gmail.com>
2025-07-13 10:41:57 +0300

    index modify and app modify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -8,20 +8,18 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen " dir="rtl">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/jobs" element={<Jobs />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen" dir="rtl">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="jobs" element={<Jobs />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
