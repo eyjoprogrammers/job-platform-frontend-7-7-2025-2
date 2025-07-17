0cf02424ea1759b68925326256f71f6229402eba
YourUsername <eyjoprogrammers@gmail.com>
2025-07-17 18:15:20 +0300

    تعديل صفحة الوظائف

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -9,7 +9,7 @@ const Jobs = () => {
   const jobsPerPage = 5;
 
   useEffect(() => {
-    fetch(process.env.PUBLIC_URL + "/data/api.json")
+    fetch("/data/api.json")
       .then((response) => {
         if (!response.ok) throw new Error("خطأ في تحميل البيانات");
         return response.json();
